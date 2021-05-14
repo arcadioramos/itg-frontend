ac708a88022420b088ac85f40a0e9d587b54bedc
Arcadio Ramos <arcadioramosisw@gmail.com>
2021-05-14 11:50:52 -0700

    make it better

--- server.js
+++ server.js
@@ -22,15 +22,15 @@ const transporter = nodemailer.createTransport(sendgridTransport({
 }))
 
 const app = express();
-app.use(cors(), function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "https://itg-done.herokuapp.com/"); // update to match the domain you will make the request from
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
+app.use(cors()); 
+  var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
     next();
-  });
-  
+};
+
+app.use(allowCrossDomain);
 app.use(fileUpload());
 app.use(cookieParser());
 
